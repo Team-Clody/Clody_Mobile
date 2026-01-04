540cddec4d58abc70285b898614751610cb8f8f5
SYAAINN <alswo08319@gmail.com>
2026-01-04 16:22:18 +0900

    [feat/#9] 루트 네비게이터 단에서 앱 전체에 System Bar Padding을 주기 위해 SafeArea 사용

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import { Routes, StackNavParamList } from './route';
+import { StyleSheet } from 'react-native';
 
 const Stack = createStackNavigator<StackNavParamList>();
 
@@ -13,11 +16,25 @@ export function RootNavigator({
   initialRoute: keyof StackNavParamList;
 }) {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={initialRoute}>
-          <Stack.Screen name={Routes.LOGIN} component={LoginScreen} />
-          <Stack.Screen name={Routes.HOME} component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName={initialRoute}>
+            <Stack.Screen
+              name={Routes.LOGIN}
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name={Routes.HOME} component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
