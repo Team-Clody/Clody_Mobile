c8b1e0ae1f0d5bc567662da18b1f73e59bce53a8
SYAAINN <alswo08319@gmail.com>
2026-01-10 02:34:00 +0900

    [chore/#15] 로깅 위치 변경

--- src/auth/kakaoAuth.ts
+++ src/auth/kakaoAuth.ts
@@ -8,14 +8,14 @@ import { login } from '@react-native-seoul/kakao-login';
 export const kakaoLogin = async (): Promise<string> => {
   try {
     const token = await login();
-    console.log('[Auth] Kakao login success', token);
 
     if (!token?.accessToken) {
       throw new Error('카카오 accessToken을 찾을 수 없습니다.');
     }
 
+    console.log('[Auth] Kakao login success', token.accessToken);
     return token.accessToken;
-  } catch (e) {
+  } catch (e: any) {
     console.error('[Auth] Kakao login error', e);
     throw e;
   }
