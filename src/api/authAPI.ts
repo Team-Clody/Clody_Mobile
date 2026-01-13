fc77c2e7bf558d59aa3e375c95145d29e1df95fc
SYAAINN <alswo08319@gmail.com>
2026-01-14 00:25:39 +0900

    [feat/#16] 구글로그인을 위한 서버통신 인터페이스 선언 - 구글로그인은 카카오, 애플로그인과 다른 API를 사용하며 해당 API는 헤더에 Authorization이 없습니다.

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -2,6 +2,7 @@ import { createAPIRequest } from '../shared/http';
 import { HeaderType } from '../shared/http';
 import { PostSigninResponseDTO } from './dto/response/postSigninReponseDTO';
 import { PostSigninRequestDTO } from './dto/request/postSigninRequestDTO';
+import { PostGoogleSigninRequestDTO } from './dto/request/postGoogleSigninRequestDTO';
 
 export const AuthAPI = {
   postSignin: async (
@@ -15,6 +16,16 @@ export const AuthAPI = {
       requestDTO,
       undefined,
       platformToken,
+    );
+    return resp.data.data;
+  },
+
+  postGoogleSignin: async (requestDTO: PostGoogleSigninRequestDTO) => {
+    const resp = await createAPIRequest<PostSigninResponseDTO>(
+      'post',
+      '/auth/oauth2/google',
+      HeaderType.NON_AUTH,
+      requestDTO,
     );
     return resp.data.data;
   },
