634463dfca0fc23f8939b4f89c69b3e6397f5394
yeonjeen <jeendev2019@gmail.com>
2026-01-15 03:51:04 +0900

    [Feat/#23] Button 컴포넌트 containerStyle prop 추가

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,23 +1,32 @@
-import { Pressable, View } from 'react-native';
+import { Pressable, StyleProp, View, ViewStyle } from 'react-native';
 import { Typo } from './Typo';
 
 interface ButtonProps {
   title: string;
   onPress: () => void;
   isDisabled?: boolean;
+  containerStyle?: StyleProp<ViewStyle>;
 }
 
-export const Button = ({ title, onPress, isDisabled }: ButtonProps) => {
+export const Button = ({
+  title,
+  onPress,
+  isDisabled,
+  containerStyle,
+}: ButtonProps) => {
   return (
     <Pressable onPress={onPress} disabled={isDisabled}>
       <View
-        style={{
-          height: 48,
-          backgroundColor: isDisabled ? '#E3E6ED' : '#293038',
-          borderRadius: 6,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+        style={[
+          {
+            height: 48,
+            backgroundColor: isDisabled ? '#E3E6ED' : '#293038',
+            borderRadius: 6,
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+          containerStyle,
+        ]}
       >
         <Typo.Body variant="body1" color={isDisabled ? '#ABAFBB' : '#FFFFFF'}>
           {title}
