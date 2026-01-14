0d48745b9d7eae4d0500a5818a0b3fb8c67b1d53
yeonjeen <jeendev2019@gmail.com>
2026-01-15 01:00:39 +0900

    [feat/#7] 바텀 탭 라우트 설정

--- src/navigation/route.ts
+++ src/navigation/route.ts
@@ -1,9 +1,18 @@
-export enum Routes {
-  LOGIN = 'login',
-  HOME = 'home',
-}
+export const Routes = {
+  LOGIN: 'Login',
+  HOME: 'Home',
+  CALENDAR: 'Calendar',
+  MY_PAGE: 'MyPage',
+  MAIN_TAB: 'MainTab',
+} as const;
 
 export type StackNavParamList = {
   [Routes.LOGIN]: undefined;
+  [Routes.MAIN_TAB]: undefined;
+};
+
+export type BottomTabParamList = {
   [Routes.HOME]: undefined;
+  [Routes.CALENDAR]: undefined;
+  [Routes.MY_PAGE]: undefined;
 };
